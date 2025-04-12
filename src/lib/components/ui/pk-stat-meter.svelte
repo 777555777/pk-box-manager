1038ba98cdd09a209cad2036597890a10c5c3234
Bjoern Crombach <bjoern.crombach95@gmail.com>
2025-04-12 14:35:41 +0200

    style: add pixel borders for base stat meter
    also change order of elements

--- src/lib/components/ui/pk-stat-meter.svelte
+++ src/lib/components/ui/pk-stat-meter.svelte
@@ -3,56 +3,72 @@
 </script>
 
 <label for="stat-{index}" class="stat-label">{label}</label>
-<meter
-	id="stat-{index}"
-	{value}
-	min="0"
-	max={maxStatValue}
-	low={maxStatValue * 0.2}
-	high={maxStatValue * 0.4}
-	optimum={maxStatValue}
-	class="stat-meter pixel-corners"
-></meter>
+
 <span class="stat-value">{value}</span>
+
+<div class="meter-border pixel-corners">
+	<meter
+		id="stat-{index}"
+		{value}
+		min="0"
+		max={maxStatValue}
+		low={maxStatValue * 0.2}
+		high={maxStatValue * 0.4}
+		optimum={maxStatValue}
+		class="pixel-meter"
+	></meter>
+</div>
 
 <style>
 	.stat-label {
-		width: 60px;
+		width: 4rem;
 		text-align: left;
-		margin-right: 8px;
-		font-weight: bold;
-	}
-
-	.stat-meter {
-		flex-grow: 1;
-		height: 20px;
 	}
 
 	.stat-value {
-		width: 40px;
 		text-align: right;
+		width: 2em;
 	}
 
 	/* Anpassung des meter-Elements */
-	meter {
-		overflow: hidden;
+
+	.pixel-meter {
+		box-sizing: border-box;
+		display: block;
+
+		background: transparent;
+		width: 12rem;
+		height: 0.75rem;
+	}
+
+	/* Make colored part as high as in Firefox */
+	/* !Deprecated https://developer.mozilla.org/en-US/docs/Web/CSS/::-webkit-meter-bar */
+	.pixel-meter::-webkit-meter-bar {
+		background: transparent;
+		height: 0.75rem;
+	}
+
+	.meter-border {
+		--meter-border-pixel-size: 3px;
+		width: fit-content;
+		height: fit-content;
+		border: var(--meter-border-pixel-size) solid rgb(35, 35, 35);
 	}
 
 	.pixel-corners {
-		--pixel-size: 2px;
 		clip-path: polygon(
-			0px calc(100% - var(--pixel-size)),
-			var(--pixel-size) calc(100% - var(--pixel-size)),
-			var(--pixel-size) 100%,
-			calc(100% - var(--pixel-size)) 100%,
-			calc(100% - var(--pixel-size)) calc(100% - var(--pixel-size)),
-			100% calc(100% - var(--pixel-size)),
-			100% var(--pixel-size),
-			calc(100% - var(--pixel-size)) var(--pixel-size),
-			calc(100% - var(--pixel-size)) 0px,
-			var(--pixel-size) 0px,
-			var(--pixel-size) var(--pixel-size),
-			0px var(--pixel-size)
+			0px calc(100% - var(--meter-border-pixel-size)),
+			var(--meter-border-pixel-size) calc(100% - var(--meter-border-pixel-size)),
+			var(--meter-border-pixel-size) 100%,
+			calc(100% - var(--meter-border-pixel-size)) 100%,
+			calc(100% - var(--meter-border-pixel-size)) calc(100% - var(--meter-border-pixel-size)),
+			100% calc(100% - var(--meter-border-pixel-size)),
+			100% var(--meter-border-pixel-size),
+			calc(100% - var(--meter-border-pixel-size)) var(--meter-border-pixel-size),
+			calc(100% - var(--meter-border-pixel-size)) 0px,
+			var(--meter-border-pixel-size) 0px,
+			var(--meter-border-pixel-size) var(--meter-border-pixel-size),
+			0px var(--meter-border-pixel-size)
 		);
 	}
 </style>
