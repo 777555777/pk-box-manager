9c6043989c3e49e84afc3b0713b7ea0677d1fdb8
Bjoern Crombach <bjoern.crombach95@gmail.com>
2025-05-03 03:18:13 +0200

    style: apply ui styling for defaults dialog

--- src/lib/components/toolbox/pk-defaults.svelte
+++ src/lib/components/toolbox/pk-defaults.svelte
@@ -7,6 +7,7 @@
 	import type { GameType } from '$lib/models/data-models'
 
 	let dialogElement: HTMLDialogElement
+	let disabled = $derived(!appState.hasModifiedDefaults())
 
 	export function showDefaultsDialog() {
 		dialogElement.showModal()
@@ -42,75 +43,150 @@
 	}
 </script>
 
-<dialog bind:this={dialogElement}>
-	<section class="pk-defaults">
-		<!-- Ball -->
-		<PkBallSelector
-			label="Ball"
-			onUpdate={updatePokeball}
-			selectedBall={appState.getAppDefaults().ball}
-		/>
+<dialog class="pk-ui-section" bind:this={dialogElement}>
+	<section class="pk-ui-section-inner">
+		<section class="pk-dialog-header">
+			<h2>Editing defaults</h2>
+			<button class="pk-button" onclick={closeDialog}><img src="ui/x-icon.png" alt="" /></button>
+		</section>
 
-		<!-- Shiny -->
-		<PkToggle
-			icon="sparkle-solid.svg"
-			activeColor="hsla(50, 80%, 30%, 0.8)"
-			label="Shiny"
-			hideLabel={true}
-			onUpdate={toggleShiny}
-			checked={appState.getAppDefaults().shiny}
-		/>
+		<div class="separator"></div>
 
-		<!-- Game -->
-		<PkGameSelector
-			label="Caught in"
-			onUpdate={updateCaughtIn}
-			value={appState.getAppDefaults().caughtIn}
-		/>
+		<section class="pk-dialog-content">
+			<section class="pk-dialog-description">
+				<p>
+					Configure default values that will be automatically applied to a Pokémon when it is first
+					marked as captured
+				</p>
+			</section>
 
-		<!-- Comment -->
-		<PkTextarea
-			label="Comment"
-			onUpdate={updateComment}
-			value={appState.getAppDefaults().comment}
-			debounceTime={250}
-		/>
+			<button class="pk-button" onclick={resetDefaults} {disabled}>Restore defaults</button>
+
+			<section class="pk-defaults">
+				<div class="pk-input-group">
+					<!-- Game -->
+					<PkGameSelector
+						label="Caught in"
+						onUpdate={updateCaughtIn}
+						value={appState.getAppDefaults().caughtIn}
+					/>
+					<div class="pk-btn-group">
+						<!-- Ball -->
+						<PkBallSelector
+							onUpdate={updatePokeball}
+							selectedBall={appState.getAppDefaults().ball}
+						/>
+
+						<!-- Shiny -->
+						<PkToggle
+							icon="sparkle-solid.svg"
+							activeColor="hsla(125, 100%, 30%, 0.55)"
+							label="Shiny"
+							hideLabel={false}
+							onUpdate={toggleShiny}
+							checked={appState.getAppDefaults().shiny}
+						/>
+					</div>
+				</div>
+
+				<!-- Comment -->
+				<PkTextarea
+					label="Comment"
+					onUpdate={updateComment}
+					value={appState.getAppDefaults().comment}
+					debounceTime={250}
+				/>
+			</section>
+		</section>
+
+		<div class="separator"></div>
+
+		<section class="pk-dialog-footer">
+			<button class="pk-button" onclick={closeDialog}>Close</button>
+		</section>
 	</section>
-	<div class="pk-default-options">
-		<button onclick={resetDefaults}>Reset</button>
-		<button onclick={closeDialog}>Close</button>
-	</div>
 </dialog>
 
 <style>
 	dialog {
-		background-color: hsl(240, 100%, 90%);
-		border: 2px solid hsl(250, 100%, 74%);
-		border-radius: 10px;
-		padding: 10px;
+		--dialog-spacing: 1rem;
+		--dialog-min-width: 680px;
 
-		margin: auto;
-		min-width: 600px;
+		position: fixed;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -65%);
+		overflow: visible;
+		min-width: var(--dialog-min-width);
+		color: var(--ui-text-color);
+		.pk-ui-section-inner {
+			padding: var(--dialog-spacing);
+		}
+	}
+
+	.pk-dialog-header {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+
+		.pk-button {
+			max-width: 44px;
+		}
+	}
+
+	.pk-dialog-content {
+		display: flex;
+		flex-direction: column;
+		gap: var(--dialog-spacing);
+		margin-bottom: calc(var(--dialog-spacing) * 2);
+
+		.pk-dialog-description {
+			margin: 0.5rem 0;
+
+			p {
+				line-height: 1.6;
+				max-width: 55ch;
+				letter-spacing: 0.02em;
+			}
+		}
+
+		.pk-button {
+			align-self: flex-start; /* Prevent restore button from taking up full row */
+		}
+
+		.pk-defaults {
+			display: flex;
+			gap: 3rem;
+		}
+
+		.pk-input-group {
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+			gap: 1rem;
+
+			.pk-btn-group {
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				gap: 1rem;
+			}
+		}
+	}
+
+	.pk-dialog-footer {
+		display: flex;
+		justify-content: flex-end;
 	}
 
 	dialog::backdrop {
 		background-color: rgba(0, 0, 0, 0.5);
 	}
 
-	.pk-defaults {
-		margin-top: 20px;
-		display: flex;
-		flex-direction: column;
-		align-items: start;
-		gap: 10px;
-	}
-
-	button {
-		padding: 0.35rem 0.25rem;
-	}
-
-	.pk-default-options {
-		display: flex;
-		justify-content: space-between;
+	.separator {
+		height: 2px;
+		background-color: rgba(0, 0, 0, 0.3);
+		margin: 1rem 0;
+		width: 100%;
 	}
 </style>
