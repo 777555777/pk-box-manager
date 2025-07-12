d770dc17db23cd454364c4f4f452cb9a2834cb21
Bjoern Crombach <bjoern.crombach95@gmail.com>
2025-07-12 02:47:53 +0200

    feat: add and build ribbon sprites

---
+++ src/lib/models/ribbons-models.ts
@@ -0,0 +1,768 @@
+// Automatically generated file!
+export const Ribbons = {
+  "001-national-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: 0,
+      y: 0
+    }
+  },
+  "002-earth-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -40,
+      y: 0
+    }
+  },
+  "003-champion-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -80,
+      y: 0
+    }
+  },
+  "004-effort-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -120,
+      y: 0
+    }
+  },
+  "005-cool-ribbon-hoenn": {
+    sheet: "sr1",
+    pos: {
+      x: -160,
+      y: 0
+    }
+  },
+  "006-cool-ribbon-super-hoenn": {
+    sheet: "sr1",
+    pos: {
+      x: -200,
+      y: 0
+    }
+  },
+  "007-cool-ribbon-hyper-hoenn": {
+    sheet: "sr1",
+    pos: {
+      x: -240,
+      y: 0
+    }
+  },
+  "008-cool-ribbon-master-hoenn": {
+    sheet: "sr1",
+    pos: {
+      x: -280,
+      y: 0
+    }
+  },
+  "009-beauty-ribbon-hoenn": {
+    sheet: "sr1",
+    pos: {
+      x: 0,
+      y: -40
+    }
+  },
+  "010-beauty-ribbon-super-hoenn": {
+    sheet: "sr1",
+    pos: {
+      x: -40,
+      y: -40
+    }
+  },
+  "011-beauty-ribbon-hyper-hoenn": {
+    sheet: "sr1",
+    pos: {
+      x: -80,
+      y: -40
+    }
+  },
+  "012-beauty-ribbon-master-hoenn": {
+    sheet: "sr1",
+    pos: {
+      x: -120,
+      y: -40
+    }
+  },
+  "013-cute-ribbon-hoenn": {
+    sheet: "sr1",
+    pos: {
+      x: -160,
+      y: -40
+    }
+  },
+  "014-cute-ribbon-super-hoenn": {
+    sheet: "sr1",
+    pos: {
+      x: -200,
+      y: -40
+    }
+  },
+  "015-cute-ribbon-hyper-hoenn": {
+    sheet: "sr1",
+    pos: {
+      x: -240,
+      y: -40
+    }
+  },
+  "016-cute-ribbon-master-hoenn": {
+    sheet: "sr1",
+    pos: {
+      x: -280,
+      y: -40
+    }
+  },
+  "017-smart-ribbon-hoenn": {
+    sheet: "sr1",
+    pos: {
+      x: 0,
+      y: -80
+    }
+  },
+  "018-smart-ribbon-super-hoenn": {
+    sheet: "sr1",
+    pos: {
+      x: -40,
+      y: -80
+    }
+  },
+  "019-smart-ribbon-hyper-hoenn": {
+    sheet: "sr1",
+    pos: {
+      x: -80,
+      y: -80
+    }
+  },
+  "020-smart-ribbon-master-hoenn": {
+    sheet: "sr1",
+    pos: {
+      x: -120,
+      y: -80
+    }
+  },
+  "021-tough-ribbon-hoenn": {
+    sheet: "sr1",
+    pos: {
+      x: -160,
+      y: -80
+    }
+  },
+  "022-tough-ribbon-super-hoenn": {
+    sheet: "sr1",
+    pos: {
+      x: -200,
+      y: -80
+    }
+  },
+  "023-tough-ribbon-hyper-hoenn": {
+    sheet: "sr1",
+    pos: {
+      x: -240,
+      y: -80
+    }
+  },
+  "024-tough-ribbon-master-hoenn": {
+    sheet: "sr1",
+    pos: {
+      x: -280,
+      y: -80
+    }
+  },
+  "025-winning-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: 0,
+      y: -120
+    }
+  },
+  "026-victory-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -40,
+      y: -120
+    }
+  },
+  "027-artist-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -80,
+      y: -120
+    }
+  },
+  "028-sinnoh-champion-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -120,
+      y: -120
+    }
+  },
+  "029-alert-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -160,
+      y: -120
+    }
+  },
+  "030-shock-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -200,
+      y: -120
+    }
+  },
+  "031-downcast-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -240,
+      y: -120
+    }
+  },
+  "032-careless-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -280,
+      y: -120
+    }
+  },
+  "033-relax-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: 0,
+      y: -160
+    }
+  },
+  "034-snooze-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -40,
+      y: -160
+    }
+  },
+  "035-smile-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -80,
+      y: -160
+    }
+  },
+  "036-gorgeous-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -120,
+      y: -160
+    }
+  },
+  "037-royal-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -160,
+      y: -160
+    }
+  },
+  "038-gorgeous-royal-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -200,
+      y: -160
+    }
+  },
+  "039-cool-ribbon-sinnoh": {
+    sheet: "sr1",
+    pos: {
+      x: -240,
+      y: -160
+    }
+  },
+  "040-cool-ribbon-great-sinnoh": {
+    sheet: "sr1",
+    pos: {
+      x: -280,
+      y: -160
+    }
+  },
+  "041-cool-ribbon-ultra-sinnoh": {
+    sheet: "sr1",
+    pos: {
+      x: 0,
+      y: -200
+    }
+  },
+  "042-cool-ribbon-master-sinnoh": {
+    sheet: "sr1",
+    pos: {
+      x: -40,
+      y: -200
+    }
+  },
+  "043-beauty-ribbon-sinnoh": {
+    sheet: "sr1",
+    pos: {
+      x: -80,
+      y: -200
+    }
+  },
+  "044-beauty-ribbon-great-sinnoh": {
+    sheet: "sr1",
+    pos: {
+      x: -120,
+      y: -200
+    }
+  },
+  "045-beauty-ribbon-ultra-sinnoh": {
+    sheet: "sr1",
+    pos: {
+      x: -160,
+      y: -200
+    }
+  },
+  "046-beauty-ribbon-master-sinnoh": {
+    sheet: "sr1",
+    pos: {
+      x: -200,
+      y: -200
+    }
+  },
+  "047-cute-ribbon-sinnoh": {
+    sheet: "sr1",
+    pos: {
+      x: -240,
+      y: -200
+    }
+  },
+  "048-cute-ribbon-great-sinnoh": {
+    sheet: "sr1",
+    pos: {
+      x: -280,
+      y: -200
+    }
+  },
+  "049-cute-ribbon-ultra-sinnoh": {
+    sheet: "sr1",
+    pos: {
+      x: 0,
+      y: -240
+    }
+  },
+  "050-cute-ribbon-master-sinnoh": {
+    sheet: "sr1",
+    pos: {
+      x: -40,
+      y: -240
+    }
+  },
+  "051-smart-ribbon-sinnoh": {
+    sheet: "sr1",
+    pos: {
+      x: -80,
+      y: -240
+    }
+  },
+  "052-smart-ribbon-great-sinnoh": {
+    sheet: "sr1",
+    pos: {
+      x: -120,
+      y: -240
+    }
+  },
+  "053-smart-ribbon-ultra-sinnoh": {
+    sheet: "sr1",
+    pos: {
+      x: -160,
+      y: -240
+    }
+  },
+  "054-smart-ribbon-master-sinnoh": {
+    sheet: "sr1",
+    pos: {
+      x: -200,
+      y: -240
+    }
+  },
+  "055-tough-ribbon-sinnoh": {
+    sheet: "sr1",
+    pos: {
+      x: -240,
+      y: -240
+    }
+  },
+  "056-tough-ribbon-great-sinnoh": {
+    sheet: "sr1",
+    pos: {
+      x: -280,
+      y: -240
+    }
+  },
+  "057-tough-ribbon-ultra-sinnoh": {
+    sheet: "sr1",
+    pos: {
+      x: 0,
+      y: -280
+    }
+  },
+  "058-tough-ribbon-master-sinnoh": {
+    sheet: "sr1",
+    pos: {
+      x: -40,
+      y: -280
+    }
+  },
+  "059-footprint-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -80,
+      y: -280
+    }
+  },
+  "060-legend-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -120,
+      y: -280
+    }
+  },
+  "061-ability-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -160,
+      y: -280
+    }
+  },
+  "062-great-ability-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -200,
+      y: -280
+    }
+  },
+  "063-double-ability-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -240,
+      y: -280
+    }
+  },
+  "064-multi-ability-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -280,
+      y: -280
+    }
+  },
+  "065-pair-ability-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: 0,
+      y: -320
+    }
+  },
+  "066-world-ability-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -40,
+      y: -320
+    }
+  },
+  "067-contest-memory-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -80,
+      y: -320
+    }
+  },
+  "068-contest-memory-ribbon-gold": {
+    sheet: "sr1",
+    pos: {
+      x: -120,
+      y: -320
+    }
+  },
+  "069-battle-memory-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -160,
+      y: -320
+    }
+  },
+  "070-battle-memory-ribbon-gold": {
+    sheet: "sr1",
+    pos: {
+      x: -200,
+      y: -320
+    }
+  },
+  "071-kalos-champion-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -240,
+      y: -320
+    }
+  },
+  "072-training-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -280,
+      y: -320
+    }
+  },
+  "073-skillful-battler-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: 0,
+      y: -360
+    }
+  },
+  "074-expert-battler-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -40,
+      y: -360
+    }
+  },
+  "075-best-friends-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -80,
+      y: -360
+    }
+  },
+  "076-coolness-master-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -120,
+      y: -360
+    }
+  },
+  "077-beauty-master-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -160,
+      y: -360
+    }
+  },
+  "078-cuteness-master-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -200,
+      y: -360
+    }
+  },
+  "079-cleverness-master-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -240,
+      y: -360
+    }
+  },
+  "080-toughness-master-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -280,
+      y: -360
+    }
+  },
+  "081-contest-star-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: 0,
+      y: -400
+    }
+  },
+  "082-hoenn-champion-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -40,
+      y: -400
+    }
+  },
+  "083-alola-champion-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -80,
+      y: -400
+    }
+  },
+  "084-battle-royal-master-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -120,
+      y: -400
+    }
+  },
+  "085-battle-tree-great-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -160,
+      y: -400
+    }
+  },
+  "086-battle-tree-master-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -200,
+      y: -400
+    }
+  },
+  "087-battle-champion-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -240,
+      y: -400
+    }
+  },
+  "088-birthday-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -280,
+      y: -400
+    }
+  },
+  "089-blue-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: 0,
+      y: -440
+    }
+  },
+  "090-carnival-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -40,
+      y: -440
+    }
+  },
+  "091-classic-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -80,
+      y: -440
+    }
+  },
+  "092-country-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -120,
+      y: -440
+    }
+  },
+  "093-event-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -160,
+      y: -440
+    }
+  },
+  "094-festival-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -200,
+      y: -440
+    }
+  },
+  "095-green-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -240,
+      y: -440
+    }
+  },
+  "096-history-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -280,
+      y: -440
+    }
+  },
+  "097-land-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: 0,
+      y: -480
+    }
+  },
+  "098-marine-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -40,
+      y: -480
+    }
+  },
+  "099-national-champion-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -80,
+      y: -480
+    }
+  },
+  "100-premier-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -120,
+      y: -480
+    }
+  },
+  "101-record-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -160,
+      y: -480
+    }
+  },
+  "102-red-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -200,
+      y: -480
+    }
+  },
+  "103-regional-champion-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -240,
+      y: -480
+    }
+  },
+  "104-sky-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -280,
+      y: -480
+    }
+  },
+  "105-souvenir-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: 0,
+      y: -520
+    }
+  },
+  "106-special-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -40,
+      y: -520
+    }
+  },
+  "107-wishing-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -80,
+      y: -520
+    }
+  },
+  "108-world-champion-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -120,
+      y: -520
+    }
+  },
+  "109-world-ribbon": {
+    sheet: "sr1",
+    pos: {
+      x: -160,
+      y: -520
+    }
+  }
+} as const;
+
+export type RibbonsType = keyof typeof Ribbons;
