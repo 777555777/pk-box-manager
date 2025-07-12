182d788036e06be73f7acb0fb2030d57604b3ac0
Bjoern Crombach <bjoern.crombach95@gmail.com>
2025-07-12 15:40:10 +0200

    feat: add mark images and spritesheet

---
+++ src/lib/models/marks-models.ts
@@ -0,0 +1,320 @@
+// Automatically generated file!
+export const Marks = {
+  "absent-minded-mark": {
+    sheet: "sm1",
+    pos: {
+      x: 0,
+      y: 0
+    }
+  },
+  "angry-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -40,
+      y: 0
+    }
+  },
+  "blizzard-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -80,
+      y: 0
+    }
+  },
+  "calmness-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -120,
+      y: 0
+    }
+  },
+  "charismatic-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -160,
+      y: 0
+    }
+  },
+  "cloudy-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -200,
+      y: 0
+    }
+  },
+  "crafty-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -240,
+      y: 0
+    }
+  },
+  "curry-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -280,
+      y: 0
+    }
+  },
+  "dawn-mark": {
+    sheet: "sm1",
+    pos: {
+      x: 0,
+      y: -40
+    }
+  },
+  "destiny-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -40,
+      y: -40
+    }
+  },
+  "dry-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -80,
+      y: -40
+    }
+  },
+  "dusk-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -120,
+      y: -40
+    }
+  },
+  "excited-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -160,
+      y: -40
+    }
+  },
+  "ferocious-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -200,
+      y: -40
+    }
+  },
+  "fishing-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -240,
+      y: -40
+    }
+  },
+  "flustered-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -280,
+      y: -40
+    }
+  },
+  "humble-mark": {
+    sheet: "sm1",
+    pos: {
+      x: 0,
+      y: -80
+    }
+  },
+  "intellectual-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -40,
+      y: -80
+    }
+  },
+  "intense-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -80,
+      y: -80
+    }
+  },
+  "jittery-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -120,
+      y: -80
+    }
+  },
+  "joyful-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -160,
+      y: -80
+    }
+  },
+  "kindly-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -200,
+      y: -80
+    }
+  },
+  "lunchtime-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -240,
+      y: -80
+    }
+  },
+  "misty-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -280,
+      y: -80
+    }
+  },
+  "peeved-mark": {
+    sheet: "sm1",
+    pos: {
+      x: 0,
+      y: -120
+    }
+  },
+  "prideful-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -40,
+      y: -120
+    }
+  },
+  "pumped-up-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -80,
+      y: -120
+    }
+  },
+  "rainy-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -120,
+      y: -120
+    }
+  },
+  "rare-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -160,
+      y: -120
+    }
+  },
+  "rowdy-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -200,
+      y: -120
+    }
+  },
+  "sandstorm-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -240,
+      y: -120
+    }
+  },
+  "scowling-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -280,
+      y: -120
+    }
+  },
+  "sleepy-time-mark": {
+    sheet: "sm1",
+    pos: {
+      x: 0,
+      y: -160
+    }
+  },
+  "slump-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -40,
+      y: -160
+    }
+  },
+  "smiley-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -80,
+      y: -160
+    }
+  },
+  "snowy-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -120,
+      y: -160
+    }
+  },
+  "stormy-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -160,
+      y: -160
+    }
+  },
+  "teary-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -200,
+      y: -160
+    }
+  },
+  "thorny-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -240,
+      y: -160
+    }
+  },
+  "uncommon-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -280,
+      y: -160
+    }
+  },
+  "unsure-mark": {
+    sheet: "sm1",
+    pos: {
+      x: 0,
+      y: -200
+    }
+  },
+  "upbeat-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -40,
+      y: -200
+    }
+  },
+  "vigor-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -80,
+      y: -200
+    }
+  },
+  "zero-energy-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -120,
+      y: -200
+    }
+  },
+  "zoned-out-mark": {
+    sheet: "sm1",
+    pos: {
+      x: -160,
+      y: -200
+    }
+  }
+} as const;
+
+export type MarksType = keyof typeof Marks;
