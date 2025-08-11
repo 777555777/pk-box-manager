b3417d7296e904db3a2e166329261b0fd325b61c
Bjoern Crombach <bjoern.crombach95@gmail.com>
2025-08-11 18:22:47 +0200

    feat: provide stats in compressable json format
    keeping old file around because of comments

--- src/routes/pkinfo/+server.ts
+++ src/routes/pkinfo/+server.ts
@@ -1,8 +1,9 @@
-import { pkStats, type PkStatsType } from './pk-stats.ts'
+import pkStats from './pk-stats.json' with { type: 'json' }
+
+// import { pkStats, type PkStatsType } from './pk-stats.ts'
 
 export function GET(request: Request) {
 	const url = new URL(request.url)
-	const identifier = url.searchParams.get('identifier')
 	const all = url.searchParams.get('all')
 
 	// Wenn 'all' Parameter gesetzt ist, gib alle Pokémon-Daten zurück
@@ -10,15 +11,7 @@ export function GET(request: Request) {
 		return new Response(JSON.stringify(pkStats), {
 			headers: {
 				'Content-Type': 'application/json'
-				// 'Cache-Control': 'public, max-age=86400'
 			}
-		})
-	}
-
-	// Einzelne Pokémon-Abfrage (bestehende Funktionalität)
-	if (identifier && pkStats[identifier as PkStatsType]) {
-		return new Response(JSON.stringify(pkStats[identifier as PkStatsType]), {
-			headers: { 'Content-Type': 'application/json' }
 		})
 	}
 
